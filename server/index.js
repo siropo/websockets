2bbf52ad360b3045073fe4c7dbe9126638c2b2c2
siropo <siropo1111@gmail.com>
2020-08-07 18:47:21 +0300

    Verfy client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
 const WebSocket = require('ws');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use('/', express.static(path.resolve(__dirname, '../client')));
+
+const server = app.listen(9876);
 
 const wss = new WebSocket.Server({
-    port: 9876
+    server,
+    verifyClient: (info) => {
+        return true;
+    }
 })
 
 wss.on('connection', function(ws) {
-    ws.send('hello from the server');
     ws.on('message', function(data) {
-        console.log(data)
-        ws.send(data)
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+              client.send(data);
+            }
+         })
     })
 })
